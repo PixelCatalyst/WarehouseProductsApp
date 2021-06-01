3e4414a8c464761c49ce08a833cfc4b7b46766a4
Marcin Kienast <kienastmarcin@gmail.com>
2021-06-01 15:48:11 +0200

    Initial commit to setup basic React and SpringBoot app

---
+++ backend/src/main/java/com/pixcat/warehouseproducts/WarehouseProductsApplication.java
@@ -0,0 +1,13 @@
+package com.pixcat.warehouseproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseProductsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/pixcat/warehouseproducts/WarehouseProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixcat.warehouseproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
