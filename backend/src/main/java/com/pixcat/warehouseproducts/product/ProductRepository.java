7313648b457346b93490d6857aa6f9f7c4851b6d
Marcin Kienast <kienastmarcin@gmail.com>
2021-06-13 16:43:32 +0200

    Extract SQL statements to static

--- backend/src/main/java/com/pixcat/warehouseproducts/product/ProductRepository.java
+++ backend/src/main/java/com/pixcat/warehouseproducts/product/ProductRepository.java
@@ -19,6 +19,42 @@ import static java.util.stream.Collectors.toList;
 
 public class ProductRepository {
 
+    private static final String SELECT_ALL_PRODUCTS_STATEMENT = "SELECT * FROM warehouse_products.details";
+    private static final String SELECT_BARCODES_FOR_ID_STATEMENT = """
+            SELECT barcode FROM warehouse_products.barcodes barcodes
+            WHERE barcodes.product_id = ?;
+            """;
+    private static final String SELECT_PRODUCT_FOR_ID_STATEMENT = """
+            SELECT * FROM warehouse_products.details details
+            WHERE details.product_id = ?
+            """;
+    private static final String SELECT_PRODUCT_ID_FOR_BARCODE_STATEMENT = """
+            SELECT product_id FROM warehouse_products.barcodes barcodes
+            WHERE barcodes.barcode = ?
+            """;
+    private static final String SELECT_PRODUCT_ID_STATEMENT = """
+            SELECT product_id FROM warehouse_products.details details
+            WHERE details.product_id = ?
+            """;
+    private static final String DELETE_BARCODES_STATEMENT = """
+            DELETE FROM warehouse_products.barcodes barcodes
+            WHERE barcodes.product_id = ?
+            """;
+    private static final String DELETE_PRODUCTS_STATEMENT = """
+            DELETE FROM warehouse_products.details details
+            WHERE details.product_id = ?
+            """;
+    private static final String INSERT_PRODUCTS_STATEMENT = """
+            INSERT INTO warehouse_products.details
+            (product_id, description, storage_temp, height, width, length, weight)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """;
+    private static final String INSERT_BARCODES_STATEMENT = """
+            INSERT INTO warehouse_products.barcodes
+            (barcode, product_id)
+            VALUES (?, ?)
+            """;
+
     private final JdbcTemplate jdbcTemplate;
 
     public ProductRepository(JdbcTemplate jdbcTemplate) {
@@ -26,7 +62,7 @@ public class ProductRepository {
     }
 
     public List<ProductDetails> getAllProducts() {
-        final var rows = jdbcTemplate.queryForList("SELECT * FROM warehouse_products.details");
+        final var rows = jdbcTemplate.queryForList(SELECT_ALL_PRODUCTS_STATEMENT);
         return rows.stream()
                 .map(row -> ProductDetails.builder()
                         .id(ProductId.of((String) row.get("product_id")))
@@ -43,12 +79,12 @@ public class ProductRepository {
     }
 
     private List<Barcode> barcodesForId(ProductId id) {
-        return jdbcTemplate.queryForList("SELECT barcode FROM warehouse_products.barcodes barcodes WHERE barcodes.product_id = ?", Barcode.class, id.value);
+        return jdbcTemplate.queryForList(SELECT_BARCODES_FOR_ID_STATEMENT, Barcode.class, id.value);
     }
 
     public ProductDetails getProductById(ProductId id) {
         try {
-            final var row = jdbcTemplate.queryForMap("SELECT * FROM warehouse_products.details dets WHERE dets.product_id = ?", id.value);
+            final var row = jdbcTemplate.queryForMap(SELECT_PRODUCT_FOR_ID_STATEMENT, id.value);
             return ProductDetails.builder()
                     .id(id)
                     .description((String) row.get("description"))
@@ -66,7 +102,8 @@ public class ProductRepository {
 
     public ProductDetails getProductByBarcode(Barcode barcode) {
         try {
-            final var id = jdbcTemplate.queryForObject("SELECT product_id FROM warehouse_products.barcodes barcodes WHERE barcodes.barcode = ?", ProductId.class, barcode.value);
+            final var id = jdbcTemplate
+                    .queryForObject(SELECT_PRODUCT_ID_FOR_BARCODE_STATEMENT, ProductId.class, barcode.value);
             if (id == null) {
                 return null;
             }
@@ -79,19 +116,19 @@ public class ProductRepository {
 
     public boolean containsProduct(ProductId id) {
         try {
-            final var str = jdbcTemplate.queryForObject("SELECT product_id FROM warehouse_products.details dets WHERE dets.product_id = ?", String.class, id.value);
+            final var foundId = jdbcTemplate.queryForObject(SELECT_PRODUCT_ID_STATEMENT, String.class, id.value);
 
-            return str != null;
+            return foundId != null;
         } catch (IncorrectResultSizeDataAccessException ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
 
     public void createProduct(ProductId id, ProductDetails details) {
-        jdbcTemplate.update("DELETE FROM warehouse_products.barcodes barcodes WHERE barcodes.product_id = ?", id.value);
-        jdbcTemplate.update("DELETE FROM warehouse_products.details dets WHERE dets.product_id = ?", id.value);
+        jdbcTemplate.update(DELETE_BARCODES_STATEMENT, id.value);
+        jdbcTemplate.update(DELETE_PRODUCTS_STATEMENT, id.value);
 
-        jdbcTemplate.update("INSERT INTO warehouse_products.details (product_id, description, storage_temp, height, width, length, weight) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        jdbcTemplate.update(INSERT_PRODUCTS_STATEMENT,
                 id.value,
                 details.getDescription(),
                 details.getStorageTemperature().toString(),
@@ -101,7 +138,7 @@ public class ProductRepository {
                 details.getWeight().value
         );
         final var barcodes = details.getBarcodes();
-        jdbcTemplate.batchUpdate("INSERT INTO warehouse_products.barcodes (barcode, product_id) VALUES (?, ?)", new BatchPreparedStatementSetter() {
+        jdbcTemplate.batchUpdate(INSERT_BARCODES_STATEMENT, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(@NotNull PreparedStatement ps, int i) throws SQLException {
                 ps.setString(1, barcodes.get(i).value);
