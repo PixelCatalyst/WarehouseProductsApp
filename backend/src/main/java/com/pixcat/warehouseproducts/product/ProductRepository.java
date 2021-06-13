8ac18cd84cf054360c9960626a48231489f8b20f
Marcin Kienast <kienastmarcin@gmail.com>
2021-06-13 15:39:16 +0200

    Add data persistence

--- backend/src/main/java/com/pixcat/warehouseproducts/product/ProductRepository.java
+++ backend/src/main/java/com/pixcat/warehouseproducts/product/ProductRepository.java
@@ -3,74 +3,113 @@ package com.pixcat.warehouseproducts.product;
 import com.pixcat.warehouseproducts.type.Kilograms;
 import com.pixcat.warehouseproducts.type.Millimeters;
 import com.pixcat.warehouseproducts.type.Temperature;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 public class ProductRepository {
 
-    private final Map<ProductId, ProductDetails> inMemoryProducts = new HashMap<>();
-
     private final JdbcTemplate jdbcTemplate;
-    // TODO Persistence
 
     public ProductRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-
-        inMemoryProducts.put(
-                ProductId.of("TEST-101"),
-                ProductDetails.builder()
-                        .id(ProductId.of("TEST-101"))
-                        .description("Can of cola")
-                        .storageTemperature(Temperature.AMBIENT)
-                        .height(Millimeters.of(20))
-                        .width(Millimeters.of(30))
-                        .length(Millimeters.of(40))
-                        .weight(Kilograms.of(BigDecimal.valueOf(0.456)))
-                        .build()
-        );
-        inMemoryProducts.put(
-                ProductId.of("TEST-102"),
-                ProductDetails.builder()
-                        .id(ProductId.of("TEST-102"))
-                        .description("Box of cornflakes")
-                        .storageTemperature(Temperature.CHILL)
-                        .height(Millimeters.of(120))
-                        .width(Millimeters.of(230))
-                        .length(Millimeters.of(340))
-                        .weight(Kilograms.of(BigDecimal.valueOf(3.456)))
-                        .build());
     }
 
     public List<ProductDetails> getAllProducts() {
-        return new ArrayList<>(inMemoryProducts.values());
+        final var rows = jdbcTemplate.queryForList("SELECT * FROM warehouse_products.details");
+        return rows.stream()
+                .map(row -> ProductDetails.builder()
+                        .id(ProductId.of((String) row.get("product_id")))
+                        .description((String) row.get("description"))
+                        .storageTemperature(Temperature.valueOf((String) row.get("storage_temp")))
+                        .height(Millimeters.of((Integer) row.get("height")))
+                        .width(Millimeters.of((Integer) row.get("width")))
+                        .length(Millimeters.of((Integer) row.get("length")))
+                        .weight(Kilograms.of((BigDecimal) row.get("weight")))
+                        .barcodes(barcodesForId(ProductId.of((String) row.get("product_id"))))
+                        .build()
+                )
+                .collect(toList());
+    }
+
+    private List<Barcode> barcodesForId(ProductId id) {
+        return jdbcTemplate.queryForList("SELECT barcode FROM warehouse_products.barcodes barcodes WHERE barcodes.product_id = ?", Barcode.class, id.value);
     }
 
     public ProductDetails getProductById(ProductId id) {
-        return inMemoryProducts.get(id);
+        try {
+            final var row = jdbcTemplate.queryForMap("SELECT * FROM warehouse_products.details dets WHERE dets.product_id = ?", id.value);
+            return ProductDetails.builder()
+                    .id(id)
+                    .description((String) row.get("description"))
+                    .storageTemperature(Temperature.valueOf((String) row.get("storage_temp")))
+                    .height(Millimeters.of((Integer) row.get("height")))
+                    .width(Millimeters.of((Integer) row.get("width")))
+                    .length(Millimeters.of((Integer) row.get("length")))
+                    .weight(Kilograms.of((BigDecimal) row.get("weight")))
+                    .barcodes(barcodesForId(id))
+                    .build();
+        } catch (IncorrectResultSizeDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     public ProductDetails getProductByBarcode(Barcode barcode) {
-        final var maybeFound = inMemoryProducts.entrySet().stream()
-                .filter(entry -> entry.getValue().getBarcodes().contains(barcode))
-                .findFirst();
+        try {
+            final var id = jdbcTemplate.queryForObject("SELECT product_id FROM warehouse_products.barcodes barcodes WHERE barcodes.barcode = ?", ProductId.class, barcode.value);
+            if (id == null) {
+                return null;
+            }
 
-        ProductDetails found = null;
-        if (maybeFound.isPresent()) {
-            found = maybeFound.get().getValue();
+            return getProductById(id);
+        } catch (IncorrectResultSizeDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return found;
     }
 
     public boolean containsProduct(ProductId id) {
-        return inMemoryProducts.containsKey(id);
+        try {
+            final var str = jdbcTemplate.queryForObject("SELECT product_id FROM warehouse_products.details dets WHERE dets.product_id = ?", String.class, id.value);
+
+            return str != null;
+        } catch (IncorrectResultSizeDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     public void createProduct(ProductId id, ProductDetails details) {
-        inMemoryProducts.put(id, details);
+        jdbcTemplate.update("INSERT INTO warehouse_products.details (product_id, description, storage_temp, height, width, length, weight) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                id.value,
+                details.getDescription(),
+                details.getStorageTemperature().toString(),
+                details.getHeight().value,
+                details.getWidth().value,
+                details.getLength().value,
+                details.getWeight().value
+        );
+
+        final var barcodes = details.getBarcodes();
+        jdbcTemplate.batchUpdate("INSERT INTO warehouse_products.barcodes (barcode, product_id) VALUES (?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(@NotNull PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, barcodes.get(i).value);
+                ps.setString(2, id.value);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return barcodes.size();
+            }
+        });
     }
 }
--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -1,5 +1,7 @@
 create schema if not exists warehouse_products;
 
+--- Users ---
+
 create table if not exists warehouse_products.users
 (
     username varchar(100) NOT NULL PRIMARY KEY,
@@ -17,4 +19,30 @@ create table if not exists warehouse_products.user_authorities
     CONSTRAINT fk_authorities
         FOREIGN KEY(username)
         REFERENCES warehouse_products.users(username)
+);
+
+--- Products ---
+
+create table if not exists warehouse_products.details
+(
+    product_id varchar(100) NOT NULL PRIMARY KEY,
+    description varchar(150),
+    storage_temp varchar(50) NOT NULL,
+    height smallint NOT NULL,
+    width smallint NOT NULL,
+    length smallint NOT NULL,
+    weight numeric NOT NULL,
+
+    CONSTRAINT measures_nonnegative CHECK (height > 0 AND width > 0 AND length > 0),
+    CONSTRAINT weight_nonnegative CHECK (weight > 0)
+);
+
+create table if not exists warehouse_products.barcodes
+(
+    barcode varchar(100) NOT NULL PRIMARY KEY,
+    product_id varchar(100) NOT NULL,
+
+    CONSTRAINT fk_barcodes
+        FOREIGN KEY(product_id)
+        REFERENCES warehouse_products.details(product_id)
 );
