bfe2e06a9d410831a49ec77dc88cb35ef6253a73
Marcin Kienast <kienastmarcin@gmail.com>
2021-08-22 23:34:21 +0200

    Return user roles on auth endpoint

--- backend/src/main/java/com/pixcat/warehouseproducts/auth/BasicAuthController.java
+++ backend/src/main/java/com/pixcat/warehouseproducts/auth/BasicAuthController.java
@@ -1,13 +1,30 @@
 package com.pixcat.warehouseproducts.auth;
 
+import com.pixcat.warehouseproducts.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
 @RestController
 public class BasicAuthController {
 
+    private final UserRepository userRepository;
+
+    public BasicAuthController(@Autowired UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping(value = "/auth")
-    public String authenticate() {
-        return "User authenticated";
+    public List<String> authenticate(Principal principal) {
+        log.info("Authentication attempt for {}", principal.getName());
+
+        return userRepository
+                .getUser(principal.getName())
+                .getRoles();
     }
 }
--- backend/src/main/java/com/pixcat/warehouseproducts/user/UserRepository.java
+++ backend/src/main/java/com/pixcat/warehouseproducts/user/UserRepository.java
@@ -1,11 +1,15 @@
 package com.pixcat.warehouseproducts.user;
 
-import com.pixcat.warehouseproducts.user.User;
+import kotlin.Pair;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toUnmodifiableList;
 
 public class UserRepository {
 
@@ -15,6 +19,13 @@ public class UserRepository {
             "INSERT INTO warehouse_products.user_authorities (username, authority) VALUES (?, ?)";
     private final static String SELECT_ALL_USERS_STATEMENT =
             "SELECT username FROM warehouse_products.users";
+    private final static String SELECT_USER_STATEMENT = """
+            SELECT users.username, users.password, auth.authority
+            FROM warehouse_products.users users
+                INNER JOIN warehouse_products.user_authorities auth
+                ON users.username = auth.username
+            WHERE users.username = ?
+            """;
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -35,6 +46,25 @@ public class UserRepository {
                     user.getUsername(),
                     "ROLE_" + role);
         }
+    }
+
+    public UserDto getUser(String username) {
+        final var rows = jdbcTemplate.queryForList(SELECT_USER_STATEMENT, username);
+        final var maybeUser = rows.stream()
+                .collect(groupingBy(r -> new Pair<>(
+                        (String) r.get("username"),
+                        (String) r.get("password")))
+                )
+                .entrySet().stream()
+                .findFirst()
+                .map(user -> new UserDto(
+                        user.getKey().getFirst(),
+                        user.getKey().getSecond(),
+                        user.getValue().stream()
+                                .map(authorities -> (String) authorities.get("authority"))
+                                .collect(toUnmodifiableList()))
+                );
+        return maybeUser.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     public List<String> getAllUsernames() {
